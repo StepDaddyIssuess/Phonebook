be926d4d1d8f7a80ec2cad716ce140555ad587c7
Kyano DeBeuckelaere <kyano.debeuckelaere12@gmail.com>
2025-04-15 17:30:58 +0200

    testy

--- models/User.js
+++ models/User.js
@@ -19,7 +19,17 @@ const userSchema = new mongoose.Schema({
         minLength: 3,
         required: true
     },
-    number: String
+    number: {
+        type: String,
+        minLength: 8,
+        validate: {
+            validator: function (v) {
+                return /\d{3}-\d{3}-\d{4}/.test(v);
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        },
+        required: [true, 'User phone number required']
+    }
 })
 
 userSchema.set("toJSON", {
